19b10cfdc9414f43f618ac14d168ea743be427de
kmtym1998 <kmtym.devloper@gmail.com>
2024-05-19 10:51:37 +0900

    BuildAuthURL をそとだし

---
+++ google/auth_endpoint.go
@@ -0,0 +1,28 @@
+package google
+
+import "net/url"
+
+func (c *Client) BuildAuthURL() *url.URL {
+	opt := c.option
+	secret := c.secret.Installed
+
+	authEndpointURL, err := url.Parse(opt.AuthEndpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	q := authEndpointURL.Query()
+	q.Add("client_id", secret.ClientID)
+	q.Add("state", opt.State)
+	q.Add("redirect_uri", opt.RedirectURI)
+	q.Add("response_type", opt.ResponseType)
+	q.Add("scope", opt.Scope)
+
+	// PKCE用パラメータ
+	q.Add("code_challenge", opt.CodeChallenge)
+	q.Add("code_challenge_method", opt.CodeChallengeMethod)
+
+	authEndpointURL.RawQuery = q.Encode()
+
+	return authEndpointURL
+}
--- handler/start.go
+++ handler/start.go
@@ -1,36 +1,17 @@
 package handler
 
 import (
-	"gphoto-dler/google"
 	"net/http"
-	"net/url"
+
+	"gphoto-dler/google"
 )
 
 // Googleの認可エンドポイントにリダイレクトさせる
 func Start(client *google.Client) func(w http.ResponseWriter, req *http.Request) {
-	opt := client.Option()
-	secret := client.Secret()
-
-	authEndpointURL, err := url.Parse(opt.AuthEndpoint)
-	if err != nil {
-		panic(err)
-	}
-
-	q := authEndpointURL.Query()
-	q.Add("client_id", secret.Installed.ClientID)
-	q.Add("state", opt.State)
-	q.Add("redirect_uri", opt.RedirectURI)
-	q.Add("response_type", opt.ResponseType)
-	q.Add("scope", opt.Scope)
-
-	// PKCE用パラメータ
-	q.Add("code_challenge", opt.CodeChallenge)
-	q.Add("code_challenge_method", opt.CodeChallengeMethod)
-
-	authEndpointURL.RawQuery = q.Encode()
+	u := client.BuildAuthURL()
 
 	// 認可エンドポイントにリダイレクト
 	return func(w http.ResponseWriter, req *http.Request) {
-		http.Redirect(w, req, authEndpointURL.String(), http.StatusFound)
+		http.Redirect(w, req, u.String(), http.StatusFound)
 	}
 }
