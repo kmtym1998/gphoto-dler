2c0ff610349f5b81f6c108e6a291cc5b269c6052
kmtym1998 <kmtym.devloper@gmail.com>
2024-05-22 01:12:42 +0900

    ファイル分割

--- google/token_request.go
+++ google/token_request.go
@@ -2,9 +2,7 @@ package google
 
 import (
 	"encoding/json"
-	"errors"
 	"io"
-	"log"
 	"log/slog"
 	"net/http"
 	"net/url"
@@ -62,40 +60,4 @@ func (c *Client) TokenRequest(code string) (*Token, error) {
 	}
 
 	return &data, nil
-}
-
-// 取得したトークンを利用してリソースにアクセス
-func (c *Client) GetMediaItems(token *Token) ([]byte, error) {
-	// NOTE: https://developers.google.com/photos/library/reference/rest/v1/mediaItems/list?hl=ja
-	const endpoint = "https://photoslibrary.googleapis.com/v1/mediaItems"
-
-	req, err := http.NewRequest(
-		http.MethodGet,
-		endpoint,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-	// 取得したアクセストークンをHeaderにセットしてリソースサーバにリクエストを送る
-	req.Header.Set("Authorization", token.TokenType+" "+token.AccessToken)
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("failed to get media items")
-	}
-
-	return body, nil
 }
